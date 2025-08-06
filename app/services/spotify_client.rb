dea94681d77b99ff1ac10324db12bcfb8f5df32a
jayastronomic <juliansmith94@hotmail.com>
2025-08-06 00:59:46 -0500

    add dot-env

--- app/services/spotify_client.rb
+++ app/services/spotify_client.rb
@@ -1,12 +1,12 @@
 require "securerandom"
-require 'net/http'
-require 'uri'
-require 'json'
-require 'base64'
+require "net/http"
+require "uri"
+require "json"
+require "base64"
 
 class SpotifyClient
-  CLIENT_ID = "601456a8e306456c81008f8264402127"
-  CLIENT_SECRET = "fa386f9bee304ea285de59487d407d60"
+  CLIENT_ID = ENV["CLIENT_ID"]
+  CLIENT_SECRET = ENV["CLIENT_SECRET"]
   REDIRECT_URI = "http://[::1]:5173/callback"
   STATE_KEY = "spotify_auth_state"
 
@@ -39,9 +39,9 @@ class SpotifyClient
 
     # Prepare POST form data
     form_data = URI.encode_www_form(
-      grant_type: 'authorization_code',
+      grant_type: "authorization_code",
       code: code,
-      redirect_uri: REDIRECT_URI
+      redirect_uri: REDIRECT_URI,
     )
 
     # Prepare Authorization header
@@ -62,8 +62,8 @@ class SpotifyClient
     JSON.parse(response.body).deep_transform_keys! { |key| key.camelize(:lower) }
   end
 
-  
   private
+
   def generate_random_string(length)
     SecureRandom.hex(60)[0, length]
   end
